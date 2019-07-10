3c61958dd1f402b4e3fe21fd82e5e3c83a672d7f
Brad <bjkapteyn@gmail.com>
2019-07-10 12:53:39 -0400

    Factoring date time method

--- AetherTest/AetherTest1.cs
+++ AetherTest/AetherTest1.cs
@@ -8,7 +8,26 @@ namespace AetherTest
         [Fact]
         public void Test1()
         {
+            string rightTime = "2019-07-09";
 
+            Assert.Equal(rightTime, GetEarlierTime(-24));
+        }
+
+        public static string GetEarlierTime(int hour)
+        {
+            DateTime now = new DateTime();
+            int minusHours = 0;
+            int minusDays = 0;
+            if((double)hour/24 >= 0)
+            {
+                minusHours = hour % 24;
+                minusDays = hour / 24;
+
+            }
+
+            string dateHour = now.AddHours(-minusHours).ToString("HH");
+
+            return before;
         }
     }
 }
